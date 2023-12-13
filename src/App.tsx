2c8462908c6ac1e9417e37b242cc782f17149fe9
강한나 <hanna0175@gmail.com>
2023-12-13 14:11:28 +0900

    🔧 add [#2]: custom color

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div>main</div>
+  return <>
+    <div className="text-white bg-main-color hover:bg-main-hover-color hover:text-black-color">메인 오렌지 색입니다.</div>
+    <div className="bg-sub-color text-main-light-color">초록색입니다.</div>
+    <div className="bg-light-gray-color text-dark-gray-color">커스텀 색깔들입니다.</div>
+  </>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 메인컬러 오렌지색
+        'main-color': '#ff5e2e',
+        'main-hover-color': '#d94e25',
+        'main-light-color': '#fff5f2',
+
+        // 서브컬러 초록색
+        'sub-color': '#3a823f',
+
+        'black-color': '#111111',
+        'light-gray-color': '#f2efef',
+        'dark-gray-color': '#999999',
+      },
+    },
   },
   plugins: [],
-};
+}
