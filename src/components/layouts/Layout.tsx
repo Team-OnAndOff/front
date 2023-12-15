a4f0f1b0fc395dcbcf476ebe4b3e8439e23c9b1f
강한나 <hanna0175@gmail.com>
2023-12-15 15:44:33 +0900

    🔧 add [#19]: 전체 레이아웃 세팅

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import { Header, Footer } from '@/components/layouts'
+import { ScrollToTop } from '@/components/common'
 
 export default function Layout() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col items-center'>
       <Header />
-      <div className='flex-1'>
+      <div className='flex-1 w-common-screen-width max-w-common-screen-width justify-self-center'>
         <Outlet />
       </div>
+      <ScrollToTop />
       <Footer />
     </div>
   )
