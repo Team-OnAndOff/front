5872316d0730fc4eef785a7e63041a2a37605dee
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-12-26 17:23:59 +0900

    🐛 fix [#34]: 전역 상태 새로 고침해도 초기화 안되도록 설정

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,18 +1,22 @@
 import { User } from '@/types'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface UserProps {
   user: User | null
   setLoginUser: (user: User) => void
 }
 
-const useAuthStore = create<UserProps>((set) => ({
-  user: null,
-  setLoginUser: (user: User) => {
-    set({ user })
-    localStorage.setItem('user', JSON.stringify(user))
-  },
-  // 여기다가 로그아웃이나 회원 탈퇴하는 부분 추가
-}))
+const useAuthStore = create(
+  persist<UserProps>(
+    (set) => ({
+      user: null,
+      setLoginUser: (user: User) => set({ user }),
+    }),
+    {
+      name: 'user',
+    },
+  ),
+)
 
 export default useAuthStore
