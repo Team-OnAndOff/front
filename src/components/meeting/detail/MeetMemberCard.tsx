0f68ee52ad30ebdafe562ae76bfb0ed7e4ffeb93
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-12-27 20:47:03 +0900

    💄 add [#48]: 상세페이지 유저 평가하기 모달 추가

--- src/components/meeting/detail/MeetMemberCard.tsx
+++ src/components/meeting/detail/MeetMemberCard.tsx
@@ -1,38 +1,66 @@
 import { EventAppliesUser } from '@/types'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import { FaRegSmile } from 'react-icons/fa'
+import { Modal } from '@/components/common'
+import Evaluation from '../mypage/Evaluation'
 interface MemberCardProps {
   member: EventAppliesUser
 }
 
 export default function MeetMeberCard({ member }: MemberCardProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+
   return (
     <>
-      <Link to={`/userinfo/${member?.user.id}`}>
-        <div className='flex mr-3 pointer flex-col p-1.5 border-2 border-dark-gray-color rounded w-60 h-28'>
+      <div className='flex mr-3 pointer flex-col p-1.5 border-2 border-dark-gray-color rounded w-60 h-28'>
+        <Link to={`/userinfo/${member?.user.id}`} className='basis-3/4'>
           <div className='basis-3/4'>
             <span className='text-size-subbody line-clamp-3'>
               {member?.user.introduction}
             </span>
           </div>
+        </Link>
 
-          <div className='flex items-center justify-between'>
-            <div className='flex items-center basis-2/3'>
+        <div className='flex items-center justify-between'>
+          <Link to={`/userinfo/${member?.user.id}`}>
+            <div className='flex items-center min-w-[160px]'>
               <img
                 alt={member?.user.username}
                 src={member?.user.image.uploadPath}
-                className='object-cover w-full mr-2 h-full max-w-[24px] max-h-[24px] rounded-big-radius'
+                className='object-cover w-full mr-1 h-full max-w-[24px] max-h-[24px] rounded-big-radius'
               />
-              <span className='text-size-subbody'>{member?.user.username}</span>
+              <span className='text-size-subbody line-clamp-1'>
+                {member?.user.username}
+              </span>
             </div>
-            <div className='w-[30px] h-[20px] bg-main-color rounded-button-radius p-1'>
+          </Link>
+          <div className='flex'>
+            <i
+              className='transition ease-in-out cursor-pointer hover:scale-110'
+              onClick={openModal}
+            >
+              <FaRegSmile size={20} />
+            </i>
+            <div className='w-[30px] h-[20px] ml-2 bg-main-color rounded-button-radius p-1'>
               <p className='font-normal text-center text-white text-size-subbody'>
                 36.5
               </p>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
+
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} closeModal={closeModal}>
+          <Evaluation
+            username={member?.user.username}
+            closeModal={closeModal}
+          />
+        </Modal>
+      )}
     </>
   )
 }
